15c14cdf633f6d25865e251a8d0ac27c202ae957
Karthik <bicskarthik@gmail.com>
2025-04-15 17:50:17 +0530

    Update adapters.py

--- adapters.py
+++ adapters.py
@@ -1,5 +1,4 @@
 from botbuilder.core import BotFrameworkAdapterSettings, BotFrameworkAdapter
-from botbuilder.core.integration import aiohttp_bot_framework_http_adapter
 from dotenv import load_dotenv
 import os
 
